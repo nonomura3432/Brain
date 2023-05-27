815db0d008a9aed413b24fe3fbcf9213aaad2782
suupia <morita777k@icloud.com>
2023-05-27 13:25:30 +0900

    A_Sc: GridPositionクラスを追加

--- Assets/EnemyGenerator.cs
+++ Assets/EnemyGenerator.cs
@@ -31,54 +31,16 @@ public class EnemyGenrator : MonoBehaviour
         {
             this.delta = 0;
 
-            int height = Screen.height;
-            float unitHeight = height / _numOfDivide;
-            
-            int randGreen = Random.Range(0, _numOfDivide);
-            float greenPosY = randGreen *unitHeight + 1/2.0f * unitHeight;
-            
-            int randRed = Random.Range(0, _numOfDivide);
-            while (randGreen == randRed)
+            var greenPos = GridPosition.GridPosByRandom(4 / 5.0f);
+            var redPos = GridPosition.GridPosByRandom(4 / 5.0f);
+            while (greenPos == redPos)
             {
-                randRed = Random.Range(0, _numOfDivide); // Greenと重ならなくなるまで再抽選
+                redPos = GridPosition.GridPosByRandom(4 / 5.0f);
             }
-            float redPosY = randRed * unitHeight + 1/2.0f * unitHeight;
-            
-            var greenVector = new Vector3(4/5.0f * Screen.width, greenPosY, 0);
-            Vector2 greenScreenPosY =  Camera.main.ScreenToWorldPoint(greenVector);  
-            var redVector = new Vector3( 4/5.0f * Screen.width, redPosY, 0);
-            Vector2 redScreenPosY =  Camera.main.ScreenToWorldPoint(redVector);
+            Instantiate(Enemy_Green, greenPos, Quaternion.identity);
+            Instantiate(Enemy_Red, redPos, Quaternion.identity);
             
             
-            Debug.Log($"greenScreenPosY = {greenScreenPosY}");
-            Debug.Log($"redScreenPosY = {redScreenPosY}");
-            Instantiate(Enemy_Green, greenScreenPosY, Quaternion.identity);
-            Instantiate(Enemy_Red, redScreenPosY, Quaternion.identity);
-            
-            
-            //  Instantiate(Enemy_Red, new Vector3(7, 3,0), Quaternion.identity);
-            //  
-            // var half = Screen.height / 2;
-            // Vector3 vector = new Vector3(7, half, 0);
-            // Vector2 screenVector = Camera.main.ScreenToWorldPoint(vector);
-            // Instantiate(Enemy_Green,screenVector , Quaternion.identity);
-
-            // float greenPy = Random.Range(-3.7f, 3.7f);
-            // int roundedGreenPy = (int)Math.Floor(greenPy);
-            // Debug.Log($"py��{roundedGreenPy}");
-            // float redPy = Random.Range(-3.7f, 3.7f);
-            // int roundedRedPy = (int)Math.Floor(redPy);
-            // Debug.Log($"py��{roundedRedPy}");
-            // //float py = Random.Range(-3.7f, 3.7f);
-            // //Math.Floor(py);
-            // //Debug.Log($"py��{py}");
-            // Vector3 greenVector = new Vector3(7, greenPy, 0);
-            // Vector3 redVector = new Vector3(7, redPy, 0);
-            // if (greenPy!=redPy)
-            // {
-            //     Instantiate(Enemy_Green, greenVector, Quaternion.identity);
-            // }
-            // Instantiate(Enemy_Red, redVector, Quaternion.identity);
         }
     }
 }
---
+++ Assets/GridPosition.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GridPosition
+{
+
+    static int _numOfDivide = 4;
+    
+    
+    public static Vector3 GridPosByRandom(float xRatio)
+    {
+        int rand = Random.Range(0, _numOfDivide);
+        return GridPos(xRatio, rand);
+    }
+    
+/// <summary>
+/// 縦に_numOfDivide分割したときの、xRatioに対応する座標を返す
+/// </summary>
+/// <param name="xRatio">画面の横に対する割合</param>
+/// <param name="yIndex">分割時の下から数えた添え字</param>
+/// <returns></returns>
+    public static Vector3 GridPos(float xRatio, int yIndex)
+    {
+        int height = Screen.height;
+        float unitHeight = height / _numOfDivide;
+        float posY = yIndex *unitHeight + 1/2.0f * unitHeight;
+        var vector = new Vector3(xRatio * Screen.width, posY, 0);
+        Vector2 screenPosY =  Camera.main.ScreenToWorldPoint(vector);  
+        return screenPosY;
+    }
+
+}
